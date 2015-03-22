55f3e90a2e1dd66a6e5b5a57051423899ab5071d
Rabindranath Prithvi <rabindranath.prithvi@gmail.com>
2015-03-22 16:31:44 +0530

    Devise User created

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'welcome/index'
+
   mount RailsAdmin::Engine => '/super_admin', as: 'rails_admin'
   comfy_route :cms_admin, :path => '/admin'
 
-  # Make sure this routeset is defined last
-  comfy_route :cms, :path => '/', :sitemap => false
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -59,4 +61,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  # Make sure this routeset is defined last
+  comfy_route :cms, :path => '/', :sitemap => false
 end
